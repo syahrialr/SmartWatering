c1260614a0045cd32ab2134e76fb3d0ff7f6e9f6
M Syahrial Rukmana <syahrial1@live.com>
2017-12-03 23:11:47 +0700

    Add Fabric Crashlytics

--- app/src/main/java/zexal/org/smartwatering/SplashScreenActivity.java
+++ app/src/main/java/zexal/org/smartwatering/SplashScreenActivity.java
@@ -3,6 +3,8 @@ package zexal.org.smartwatering;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 
 
@@ -14,6 +16,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
             startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
             finish();
         }
